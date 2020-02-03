56e2568e7b7eea6b04f51db06125e8752ad642e1
koishi <qwq@koishi.top>
2020-02-03 12:12:11 +0800

    opt

--- src/hydrant/shaders/chebyshev_shader.hpp
+++ src/hydrant/shaders/chebyshev_shader.hpp
@@ -7,32 +7,40 @@ VM_EXPORT
 	template <typename Integrator>
 	struct ChebyshevShader : Integrator
 	{
+		__device__ float
+		  skip_nblock_steps( Ray &ray, glm::vec3 const &ip,
+							 float nblocks, float cdu, float step ) const
+		{
+			float tnear, tfar;
+			ray.intersect( Box3D{ ip, ip + 1.f }, tnear, tfar );
+			auto di = ceil( ( tfar + ( nblocks - 1 ) * cdu ) / step );
+			ray.o += ray.d * di * step;
+			return di;
+		}
+
 		__device__ void
-		  apply( Ray const &ray, typename Integrator::Pixel &pixel ) const
+		  apply( Ray const &ray_in, typename Integrator::Pixel &pixel_in ) const
 		{
 			const auto step = 1e-2f * th_4;
-			const auto cdu = 1.f / glm::compMax( glm::abs( ray.d ) );
+			const auto cdu = 1.f / glm::compMax( glm::abs( ray_in.d ) );
 
-			auto pixel_reg = pixel;
+			auto ray = ray_in;
+			auto pixel = pixel_in;
 			float tnear, tfar;
 			if ( ray.intersect( bbox, tnear, tfar ) ) {
+				ray.o += ray.d * tnear;
 				const int nsteps = min( max_steps, int( ( tfar - tnear ) / step ) );
-				auto p = ray.o + ray.d * tnear;
 				for ( int i = 0; i < nsteps; ++i ) {
-					p += ray.d * step;
-					glm::vec3 ip = floor( p );
+					ray.o += ray.d * step;
+					glm::vec3 ip = floor( ray.o );
 					if ( float cd = this->chebyshev( ip ) ) {
-						float tnear, tfar;
-						Ray{ p, ray.d }.intersect( Box3D{ ip, ip + 1.f }, tnear, tfar );
-						auto d = tfar + ( cd - 1 ) * cdu;
-						i += d / step;
-						p += ray.d * d;
-					} else if ( !this->integrate( p, ip, pixel_reg ) ) {
+						i += skip_nblock_steps( ray, ip, cd, cdu, step );
+					} else if ( !this->integrate( ray.o, ip, pixel ) ) {
 						break;
 					}
 				}
 			}
-			pixel = pixel_reg;
+			pixel_in = pixel;
 		}
 
 	public:
