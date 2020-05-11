35be361815c9958a7f5f6427a86fcaac71144909
koishi <qwq@koishi.top>
2020-05-11 10:54:01 +0000

    asd

--- build_dataset.sh
+++ build_dataset.sh
@@ -85,13 +85,13 @@ lvl_arch() {
             -i ${lvl_raw_path} \
             -x $2 -y $3 -z $4 \
             -p ${pad} -s ${log_bs} \
-        >> ${tmp_dir}/archiver.log
+        | tee ${tmp_dir}/archiver.log
 
     ${toolkit_dir}/archive-thumbnailer \
             -o ${dst_dir} \
             -i ${dst_dir}/${lvl_raw_base%.*}_$((1 << ${log_bs}))p${pad}.h264 \
             -x chebyshev \
-        >> ${tmp_dir}/archive-thumbnailer.log
+        | tee ${tmp_dir}/archive-thumbnailer.log
 }
 
 # set -x
@@ -110,7 +110,7 @@ do
     echo "sampling level $i/${lvls}... [$xx, $yy, $zz] = $mb MB"
 
     ${toolkit_dir}/downsampler -i ${raw_path} -x $dx -y $dy -z $dz -s $i -o ${tmp_dir}\
-        >> ${tmp_dir}/downsampler.log
+        | tee ${tmp_dir}/downsampler.log
 
     lvl_raw_path=${tmp_dir}/${raw_base%.*}_x$((1 << $i)).raw
 
@@ -121,7 +121,7 @@ echo "writing package meta..."
 
 ${toolkit_dir}/meta-builder \
         -d ${dst_dir} \
-    >> ${tmp_dir}/meta-builder.log
+    | tee ${tmp_dir}/meta-builder.log
 
 # set +x
 
